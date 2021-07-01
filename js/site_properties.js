b498d8efc3aa463437d8122e9888b697e16d14d7
ShivKokkula <shivbk987@gmail.com>
2021-07-02 00:17:08 +0530

    Refactor - retrieve data from JSON server

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,5 @@
 let site_properties = {
-    use_local_storage : "true",
+    use_local_storage : "false",
     home_page : "../pages/home.html",
     adddress_book_form_page : "../pages/address_book_form.html",
     server_url : "http://localhost:3000/AddressBookDB/"
